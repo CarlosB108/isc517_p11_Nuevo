3161fc2fb0c3af07f82eac769afd6336efbaa8ee
Carlos <carlosbatista108@hotmail.com>
2016-10-25 15:08:20 -0400

    first commit

---
+++ src/main/java/com/neuronageek/SpringVaadinCalendarApplication.java
@@ -0,0 +1,12 @@
+package com.neuronageek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVaadinCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVaadinCalendarApplication.class, args);
+	}
+}
